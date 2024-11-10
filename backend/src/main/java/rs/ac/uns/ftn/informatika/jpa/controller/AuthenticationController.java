4db073fb335c03eb5da627b7efedab7d272b6bcd
bteodora <149783651+bteodora@users.noreply.github.com>
2024-11-10 17:02:34 +0100

    Create AuthenticationController.java

---
+++ backend/src/main/java/rs/ac/uns/ftn/informatika/jpa/controller/AuthenticationController.java
@@ -0,0 +1,76 @@
+package rs.ac.uns.ftn.informatika.jpa.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import rs.ac.uns.ftn.informatika.jpa.dto.util.JwtAuthenticationRequest;
+import rs.ac.uns.ftn.informatika.jpa.dto.util.UserRequest;
+import rs.ac.uns.ftn.informatika.jpa.dto.util.UserTokenState;
+import rs.ac.uns.ftn.informatika.jpa.exception.ResourceConflictException;
+import rs.ac.uns.ftn.informatika.jpa.model.Profile;
+import rs.ac.uns.ftn.informatika.jpa.service.ProfileService;
+import rs.ac.uns.ftn.informatika.jpa.utils.TokenUtils;
+
+//Kontroler zaduzen za autentifikaciju korisnika
+@RestController
+@RequestMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AuthenticationController {
+
+    @Autowired
+    private TokenUtils tokenUtils;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private ProfileService userService;
+
+    // Prvi endpoint koji pogadja korisnik kada se loguje.
+    // Tada zna samo svoje korisnicko ime i lozinku i to prosledjuje na backend.
+    @PostMapping("/login")
+    public ResponseEntity<UserTokenState> createAuthenticationToken(
+            @RequestBody JwtAuthenticationRequest authenticationRequest, HttpServletResponse response) {
+        // Ukoliko kredencijali nisu ispravni, logovanje nece biti uspesno, desice se
+        // AuthenticationException
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+
+        // Ukoliko je autentifikacija uspesna, ubaci korisnika u trenutni security
+        // kontekst
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // Kreiraj token za tog korisnika
+        Profile user = (Profile) authentication.getPrincipal();
+        String jwt = tokenUtils.generateToken(user.getUsername(), user.getRole());
+        int expiresIn = tokenUtils.getExpiredIn();
+
+        // Vrati token kao odgovor na uspesnu autentifikaciju
+        return ResponseEntity.ok(new UserTokenState(jwt, expiresIn));
+    }
+
+    // Endpoint za registraciju novog korisnika
+    @PostMapping("/signup")
+    public ResponseEntity<Profile> addUser(@RequestBody UserRequest userRequest, UriComponentsBuilder ucBuilder) {
+        Profile existUser = this.userService.findByUsername(userRequest.getUsername());
+
+        if (existUser != null) {
+            throw new ResourceConflictException(userRequest.getId(), "Username already exists");
+        }
+
+        Profile user = this.userService.saveProfile(userRequest);
+
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+}
