2f4907d22b7a62d8dbc8b15373348dc02735bfcb
bteodora <149783651+bteodora@users.noreply.github.com>
2024-11-12 13:56:16 +0100

    Added routes and imports

--- frontend/RabbitApp/src/app/app.module.ts
+++ frontend/RabbitApp/src/app/app.module.ts
@@ -15,7 +15,7 @@ import { MatCardModule } from '@angular/material/card';
 import { CreatePostComponent } from './pages/create-post/create-post.component';
 import { MapComponent } from './pages/map/map.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-
+import { LoginComponent } from './pages/login/login.component';
 
 
 
--- frontend/RabbitApp/src/app/app.routes.ts
+++ frontend/RabbitApp/src/app/app.routes.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { ViewPostsRegisteredComponent } from './pages/view-posts-registered/view-posts-registered.component';
 import { CreatePostComponent } from './pages/create-post/create-post.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
     { path: 'view-profile', component: ViewProfileComponent },
     { path: 'view-posts', component: ViewPostsRegisteredComponent },
     { path: 'create-post', component: CreatePostComponent},
-    { path: '', component: HomePageComponent}
+    { path: '', component: HomePageComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'signup', component: SignupComponent}
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
