eca5d26028e31111df935a25466691922eb9b2b0
bteodora <149783651+bteodora@users.noreply.github.com>
2025-07-16 14:14:23 +0200

    feat: implemented custom MQs

--- backend/src/main/java/rs/ac/uns/ftn/informatika/jpa/service/PostPublisherService.java
+++ backend/src/main/java/rs/ac/uns/ftn/informatika/jpa/service/PostPublisherService.java
@@ -1,22 +1,17 @@
 package rs.ac.uns.ftn.informatika.jpa.service;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import rs.ac.uns.ftn.informatika.jpa.dto.PostAdDTO;
+import rs.ac.uns.ftn.informatika.jpa.service.inferface.PostPublisher;
 
 @Service
 public class PostPublisherService {
+    private final PostPublisher publisher;
 
-    private final RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    public PostPublisherService(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
+    public PostPublisherService(PostPublisher publisher) {
+        this.publisher = publisher;
     }
 
-    public void sendPostForAds(String postAd)
-    {
-        rabbitTemplate.convertAndSend("advertising_exchange", "", postAd);
+    public void sendPostForAds(String postAd) {
+        publisher.publish(postAd);
     }
 }
